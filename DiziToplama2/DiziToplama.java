434315bf3c59a0308f9c8d4421b360cca319e030
Arda Can Uysal <ardacanuysal03@gmail.com>
2023-01-13 15:45:57 +0300

    2
    2

---
+++ DiziToplama2/DiziToplama.java
@@ -0,0 +1,15 @@
+package DiziToplama2;
+
+public class DiziToplama {
+    public static void main(String[] args) {
+        double b=0;
+        for (double i = 1; i < 100;) {
+            double a = (i)/(i+2);
+            i += 2;
+            b +=a;
+            System.out.println(a);
+        }
+        System.out.println(b);
+    }
+}
+
